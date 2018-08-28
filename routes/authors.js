a5fdf155fb6fc9cb4b0026943b797a96f1dd375d
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-08-28 15:47:42 -0600

    updated books

--- routes/authors.js
+++ routes/authors.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 /* eslint-env node */
 /* eslint-disable no-use-before-define */
 
@@ -8,14 +8,20 @@ let config = require('../knexfile')[env];
 let knex = require('knex')(config);
 
 router.get('/authors', (req, res, next) => {
-  knex('authors')
-  .returning('*')
-  .then((authors) => {
-      res.render('authors', {authors});
-  })
-  .catch((err) => {
-      next(err);
-  });
+	knex('authors')
+		.select('authors.first_name', 'authors.last_name', 'authors.bio', 'authors.portrait_url', 'books.title')
+		.from('authors')
+		.innerJoin('author_of_books', 'authors.id', 'author_of_books.authors_id')
+		.innerJoin('books', 'books.id', 'author_of_books.books_id')
+		.returning('*')
+		.then((authors) => {
+			res.render('authors', {
+				authors
+			});
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 /* eslint-env node */
 /* eslint-disable no-use-before-define */
 
@@ -8,14 +8,20 @@ let config = require('../knexfile')[env];
 let knex = require('knex')(config);
 
 router.get('/books', (req, res, next) => {
-  knex('books')
-  .returning('*')
-  .then((books) => {
-      res.render('books', {books});
-  })
-  .catch((err) => {
-      next(err);
-  });
+	knex('books')
+		.select('books.cover_url', 'books.title', 'authors.first_name', 'authors.last_name', 'books.genre', 'books.description')
+		.from('books')
+		.innerJoin('author_of_books', 'books.id', 'author_of_books.books_id')
+		.innerJoin('authors', 'authors.id', 'author_of_books.authors_id')
+		.returning('*')
+		.then((books) => {
+			res.render('books', {
+				books
+			});
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 module.exports = router;
