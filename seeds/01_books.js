1b3c81573eefeecbba4350e205cedb2f6bce4a20
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-08-28 11:27:55 -0600

    updated styling

--- seeds/01_books.js
+++ seeds/01_books.js
@@ -8,7 +8,7 @@ exports.seed = function (knex, Promise) {
           title: 'Python In A Nutshell',
           genre: 'Python',
           description: "This book offers Python programmers one place to look when they need help remembering or deciphering the syntax of this open source language and its many powerful but scantily documented modules. This comprehensive reference guide makes it easy to look up the most frequently needed information--not just about the Python language itself, but also the most frequently used parts of the standard library and the most important third-party extensions.",
-          cover_url: "https://s3-us-west-2.amazonaws.com/assessment-images/galvanize_reads/photos/python_in_a_nutshell.jpg,Alex,Martelli"
+          cover_url: "https://s3-us-west-2.amazonaws.com/assessment-images/galvanize_reads/photos/python_in_a_nutshell.jpg"
         },
         {
           title: 'Think Python',
