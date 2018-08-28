70461cef880a82bb998435feb4a11c200f2d0f07
Ryan Borgstrom <Rkborgstrom@mix.wvu.edu>
2018-08-28 12:44:07 -0600

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -24,11 +24,9 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    client: 'pg',
+    connection: 'postgres://lnpxrlbmxxcofq:c23744949f545ce92dccf6a687607d12ecd743c8ddc317fdf4c435b0b1b0017d@ec2-54-235-86-226.compute-1.amazonaws.com:5432/dbn4h136c6jln7'
+    
     },
     pool: {
       min: 2,
@@ -39,4 +37,3 @@ module.exports = {
     }
   }
 
-};
